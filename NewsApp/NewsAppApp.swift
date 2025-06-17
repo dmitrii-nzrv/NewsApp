3159b91700d675312db74c222ca9ec25b747b358
Dmitrii N <letsgetstarted.dmitrii@gmail.com>
2025-06-17 19:54:40 +0300

    first commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Dmitrii Nazarov on 17.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
